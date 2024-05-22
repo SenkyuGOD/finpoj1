70ed9f23ebd334e85c28fc074ccef334b5881386
Senkyu <148489749+SenkyuGOD@users.noreply.github.com>
2024-05-22 19:48:25 +0300

    Delete src/main directory

--- src/main/java/edu/training/web/newsproject/controller/concrete/CommandName.java
+++
@@ -1,9 +0,0 @@
-package edu.training.web.newsproject.controller.concrete;
-
-public enum CommandName {
-    DO_AUTH, DO_REGISTRATION, DO_LOGOUT,
-
-    GO_TO_INDEX_PAGE, GO_TO_REGISTRATION_PAGE, GO_TO_NEWS_PAGE, GO_TO_AUTH_PAGE, GO_TO_PROFILE_PAGE,
-
-    NO_SUCH_COMMAND
-}
